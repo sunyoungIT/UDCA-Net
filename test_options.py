62e756d2ddcbae31cf76d870bcc4f11fac6d2825
Sun-Young Jeon <sunyounge_@naver.com>
2026-03-04 21:06:56 +0900

    Add files via upload

---
+++ test_options.py
@@ -0,0 +1,22 @@
+from .base_options import BaseOptions
+
+
+class TestOptions(BaseOptions):
+    """This class includes test options.
+
+    It also includes shared options defined in BaseOptions.
+    """
+
+    def initialize(self, parser):
+        parser = BaseOptions.initialize(self, parser)  # define shared options
+        parser.set_defaults(load_epoch='best')
+        parser.add_argument('--url', default=False, action='store_true',
+            help='download checkpoint from url')
+        parser.add_argument('--ensemble', default=False, action='store_true',
+            help='do self ensemble')
+
+        parser.add_argument('--test_results_dir', type=str, default=None,
+            help='saves results here.')
+
+        self.is_train = False
+        return parser
